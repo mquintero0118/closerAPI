ea157e78c57ac0bef98a4d083afe69e460b28990
Alex <m.alejandro.quintero@gmail.com>
2023-11-14 09:42:52 -0600

    fist commit

---
+++ src/main/java/com/salesass/closer/CloserApplication.java
@@ -0,0 +1,13 @@
+package com.salesass.closer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salesass/closer/CloserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesass.closer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
